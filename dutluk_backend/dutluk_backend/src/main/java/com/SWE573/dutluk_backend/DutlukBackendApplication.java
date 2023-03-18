2b8b643a65e69b595b02c70cd8baa96088d37e92
Hasan Deniz Dogan <hasandenizdogan@gmail.com>
2023-03-18 15:42:02 +0300

    repository cloned. Closes #9

---
+++ dutluk_backend/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/DutlukBackendApplication.java
@@ -0,0 +1,13 @@
+package com.SWE573.dutluk_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutlukBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DutlukBackendApplication.class, args);
+	}
+
+}
---
+++ dutluk_backend/dutluk_backend/src/test/java/com/SWE573/dutluk_backend/DutlukBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SWE573.dutluk_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DutlukBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
