1edd2679c765c0252281a45c43d0e378a3dc07a7
Hasan Deniz Dogan <hasandenizdogan@gmail.com>
2023-04-29 23:46:51 +0300

    added method findByUserId to the StoryRepository. Closes #32

--- dutluk/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/repository/StoryRepository.java
+++ dutluk/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/repository/StoryRepository.java
@@ -3,5 +3,9 @@ package com.SWE573.dutluk_backend.repository;
 import com.SWE573.dutluk_backend.model.Story;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface StoryRepository extends CrudRepository<Story,Long> {
+    List<Story> findAll();
+    List<Story> findByUserId(Long userId);
 }
