9ffa84e8b637532a18096853722fecf077193c75
Hasan Deniz Dogan <hasandenizdogan@gmail.com>
2023-04-30 00:01:12 +0300

    Comment class has been reformed to meet needs of entity relationship. Closes #37

--- dutluk/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/model/Comment.java
+++ dutluk/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/model/Comment.java
@@ -1,10 +1,14 @@
 package com.SWE573.dutluk_backend.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name="comments")
@@ -12,9 +16,23 @@ import lombok.Setter;
 @Setter
 public class Comment extends BaseEntity{
 
-    @Column(name = "user_id")
-    private Long userId;
+    @NotBlank
+    @Column(columnDefinition = "TEXT")
+    private String text;
 
-    @Column(name = "comment_text")
-    private String commentText;
+    @JsonIgnoreProperties(value = {"followers", "email", "password", "biography", "stories", "following"})
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "story_id", nullable = false)
+    private Story story;
+
+
+
+    @Column
+    private Set<Long> likes = new HashSet<>();
+
 }
