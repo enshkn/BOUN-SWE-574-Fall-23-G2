1ec4046b12246fad5937ab1c95e295eccfb074b2
Hasan Deniz Dogan <hasandenizdogan@gmail.com>
2023-04-29 23:42:29 +0300

    Deleted Label and Media classes. Closes #29

--- dutluk/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/controller/TimeController.java
+++
@@ -1,9 +0,0 @@
-package com.SWE573.dutluk_backend.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/time")
-public class TimeController {
-}
