cb9a4064906f8bf697789e2069e8c4947045fbe7
Hasan Deniz Dogan <hasandenizdogan@gmail.com>
2023-04-29 23:48:31 +0300

    decided to not to go through this approach. Closes #19

--- dutluk/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/model/Time.java
+++ dutluk/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/model/Time.java
@@ -1,13 +1,10 @@
 package com.SWE573.dutluk_backend.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDate;
 
 @Entity
 @Table(name="times")
@@ -18,17 +15,6 @@ public class Time extends BaseEntity{
     private Long storyId;
     @Column(name="time_type_flag")
     private int timeTypeFlag;// specific date = 0,start&end date=1,decade=2
-
-
-    private Long createdTimeStamp = System.currentTimeMillis();
-
-    @JsonFormat(pattern = "dd/MM/yyyy")
-    @Column(name="start_time_stamp")
-    private LocalDate startTimeStamp;
-
-    @JsonFormat(pattern = "dd/MM/yyyy")
-    @Column(name="end_time_stamp")
-    private LocalDate endTimeStamp;
 
 
 }
