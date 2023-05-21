d22bd075d6490777a77375ca1a0c9cd00189e06a
Hasan Deniz Dogan <hasandenizdogan@gmail.com>
2023-05-21 19:47:29 +0300

    add and like endpoints are added. Closes #49

--- dutluk/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/controller/CommentController.java
+++ dutluk/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/controller/CommentController.java
@@ -1,11 +1,44 @@
 package com.SWE573.dutluk_backend.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.SWE573.dutluk_backend.model.Comment;
+import com.SWE573.dutluk_backend.model.Story;
+import com.SWE573.dutluk_backend.model.User;
+import com.SWE573.dutluk_backend.request.CommentRequest;
+import com.SWE573.dutluk_backend.request.LikeRequest;
+import com.SWE573.dutluk_backend.service.CommentService;
+import com.SWE573.dutluk_backend.service.StoryService;
+import com.SWE573.dutluk_backend.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.yaml.snakeyaml.tokens.CommentToken;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/comment")
 @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class CommentController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private StoryService storyService;
+
+    @Autowired
+    private CommentService commentService;
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addComment(@RequestBody CommentRequest commentRequest, HttpServletRequest request){
+        User user = userService.validateTokenizedUser(request);
+        Story story = storyService.getStoryByStoryId(commentRequest.getStoryId());
+        return ResponseEntity.ok(commentService.createComment(commentRequest,user,story));
+    }
+    @PostMapping("/like/")
+    public ResponseEntity<?> likeComment(@RequestBody LikeRequest likeRequest, HttpServletRequest request){
+        User tokenizedUser = userService.validateTokenizedUser(request);
+        return ResponseEntity.ok(commentService.likeComment(likeRequest.getLikedEntityId(),tokenizedUser.getId()));
+    }
 }
