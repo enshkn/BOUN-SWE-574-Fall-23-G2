45e76fc9f1a2c3943f4e047f117858fa32dc1392
Hasan Deniz Dogan <hasandenizdogan@gmail.com>
2023-05-21 19:46:55 +0300

    corsconfig added. Closes #49

---
+++ dutluk/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/configuration/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.SWE573.dutluk_backend.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
