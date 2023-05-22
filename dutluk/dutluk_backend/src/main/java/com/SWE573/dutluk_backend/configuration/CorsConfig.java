05244a86c4576366f7c901df010979a421dfdd7f
Hasan Deniz Dogan <hasandenizdogan@gmail.com>
2023-05-22 20:14:14 +0300

    CorsConfig and application.properties are modified Closes #64

--- dutluk/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/configuration/CorsConfig.java
+++ dutluk/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/configuration/CorsConfig.java
@@ -1,7 +1,6 @@
 package com.SWE573.dutluk_backend.configuration;
 
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,16 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Value("${FRONTEND_LINK}")
-    private String FRONTEND_LINK;
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://"+FRONTEND_LINK+":3000")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST")
                         .allowCredentials(true);
             }
