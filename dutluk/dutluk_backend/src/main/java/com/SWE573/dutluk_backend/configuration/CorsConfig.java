bb4952da76616a74bf14f9a11bf622da7130e834
Hasan Deniz Dogan <hasandenizdogan@gmail.com>
2023-05-23 13:47:50 +0300

    CorsConfig  remodified Closes #69

--- dutluk/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/configuration/CorsConfig.java
+++ dutluk/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/configuration/CorsConfig.java
@@ -1,25 +1,27 @@
 package com.SWE573.dutluk_backend.configuration;
 
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
-            }
-        };
+    @Value("${FRONTEND}")
+    private String FRONTEND;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://"+FRONTEND+":3000")
+                .allowedMethods("GET", "POST")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
     }
 }
+
+
 
