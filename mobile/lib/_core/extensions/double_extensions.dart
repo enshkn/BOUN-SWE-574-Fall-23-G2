d4f591a9eec01d0a5fa0d0497a27b0addf154f62
Hasan Deniz Dogan <hasandenizdogan@gmail.com>
2023-10-12 14:51:33 +0300

    initiated backend development branch closes #15

--- mobile/lib/_core/extensions/double_extensions.dart
+++
@@ -1,5 +0,0 @@
-import 'dart:io';
-
-extension DoubleExtensions on double {
-  double get correctSize => Platform.isIOS ? this : this * 0.85;
-}
--- mobile/lib/_domain/auth/i_auth_repository.dart
+++
@@ -1,6 +0,0 @@
-import '../../_core/classes/typedefs.dart';
-import 'model/user.dart';
-
-abstract interface class IAuthRepository {
-  EitherFuture<User> login(String email, String password);
-}
