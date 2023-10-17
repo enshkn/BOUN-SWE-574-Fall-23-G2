eb66f03265834a8e487e984967394248ef02df3e
amineglr <aminedglrglr@gmail.com>
2023-10-17 15:23:47 +0300

    delete mobile folder

--- mobile/lib/_domain/profile/i_profile_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:swe/_core/classes/typedefs.dart';
-import 'package:swe/_core/classes/typedefs.dart';
-import 'package:swe/_domain/auth/model/user.dart';
-import 'package:swe/_domain/story/model/story_model.dart';
-
-abstract interface class IProfileRepository {
-  EitherFuture<User> getUserInfo();
-}
