de61c066a6f2a7c671ccb609d1a99a47eb9e0ed6
amineglr <aminedglrglr@gmail.com>
2023-10-17 13:16:47 +0300

    delete mobile folder

--- mobile/lib/_domain/story/i_story_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:swe/_core/classes/typedefs.dart';
-import 'package:swe/_domain/story/model/story_model.dart';
-
-abstract interface class IStoryRepository {
-  EitherFuture<List<StoryModel>> getAllStory();
-  EitherFuture<List<StoryModel>> getActivityFeed();
-  EitherFuture<List<StoryModel>> getFallowedStories();
-}
