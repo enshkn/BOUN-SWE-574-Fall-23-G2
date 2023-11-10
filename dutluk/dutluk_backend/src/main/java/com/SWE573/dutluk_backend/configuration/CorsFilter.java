87b7fe06dcde95a7cbd5dd10b1581d7501233c81
Hasan Deniz Dogan <hasandenizdogan@gmail.com>
2023-11-10 11:14:27 +0300

    CorsFilter added

---
+++ dutluk/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/configuration/CorsFilter.java
@@ -0,0 +1,35 @@
+package com.SWE573.dutluk_backend.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@Component
+@WebFilter("/*")
+public class CorsFilter implements Filter {
+
+    private static final Logger logger = LoggerFactory.getLogger(CorsFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        String serverName = request.getServerName();
+        Integer serverPortInt = request.getServerPort();
+
+        logger.info("Server Name: {}", serverName+":"+serverPortInt);
+
+        chain.doFilter(request, response);
+    }
+}
+
+
