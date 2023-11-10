2c9d4b0d46bb66520bf5456f35d6f300ef797e77
Hasan Deniz Dogan <hasandenizdogan@gmail.com>
2023-11-10 11:43:44 +0300

    CorsConfig reconfigured

--- dutluk/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/configuration/CorsConfig.java
+++ dutluk/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/configuration/CorsConfig.java
@@ -1,21 +1,24 @@
 package com.SWE573.dutluk_backend.configuration;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+    @Value("${FRONTEND_URL}")
+    String FRONTEND_URL;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOriginPatterns("*")
+                .allowedOrigins(FRONTEND_URL)
                 .allowedMethods("*")
                 .allowCredentials(true)
                 .exposedHeaders("Set-Cookie")
-                .allowedOrigins("*")
                 .allowedHeaders("*");
     }
 }
