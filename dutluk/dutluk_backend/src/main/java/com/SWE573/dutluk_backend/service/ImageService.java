5f69f2c3272e17cb5a646c8bce81fea80a0adbbe
Hasan Deniz Dogan <hasandenizdogan@gmail.com>
2023-11-20 00:39:57 +0300

    story images resized to 50 kbs Closes #167

--- dutluk/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/service/ImageService.java
+++ dutluk/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/service/ImageService.java
@@ -3,6 +3,11 @@ package com.SWE573.dutluk_backend.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Base64;
 import java.util.regex.Matcher;
@@ -18,6 +23,8 @@ import org.springframework.core.io.ByteArrayResource;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import javax.imageio.ImageIO;
+
 @Service
 public class ImageService {
     @Value("${IMGUR_CLIENT_ID}")
@@ -32,12 +39,34 @@ public class ImageService {
         while (matcher.find()) {
             String base64Data = matcher.group(1);
             byte[] imageBytes = Base64.getDecoder().decode(base64Data);
-            String imgurUrl = uploadImageToImgur(imageBytes, imgurClientId);
+            int targetFileSizeKB = 50;
+            byte[] resizedImageBytes = resizeImage(imageBytes, targetFileSizeKB);
+            String imgurUrl = uploadImageToImgur(resizedImageBytes, imgurClientId);
             matcher.appendReplacement(updatedText, imgurUrl);
         }
         matcher.appendTail(updatedText);
 
         return updatedText.toString();
+    }
+
+    private byte[] resizeImage(byte[] imageBytes, int targetFileSizeKB) throws IOException {
+        ByteArrayInputStream bis = new ByteArrayInputStream(imageBytes);
+        BufferedImage bufferedImage = ImageIO.read(bis);
+
+        int originalSizeKB = imageBytes.length / 1024;
+        double scalingFactor = Math.sqrt((double) targetFileSizeKB / originalSizeKB);
+
+        int newWidth = (int) (bufferedImage.getWidth() * scalingFactor);
+        int newHeight = (int) (bufferedImage.getHeight() * scalingFactor);
+
+        Image scaledImage = bufferedImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+        BufferedImage resizedImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+        resizedImage.getGraphics().drawImage(scaledImage, 0, 0, null);
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ImageIO.write(resizedImage, "jpg", bos);
+
+        return bos.toByteArray();
     }
 
     private String uploadImageToImgur(byte[] imageBytes, String imgurClientId) throws IOException {
