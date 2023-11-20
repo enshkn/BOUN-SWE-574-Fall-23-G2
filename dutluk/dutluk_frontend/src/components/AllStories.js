59dbba96870989d08cdabbebc786b14541b9c5b1
Gökalp Ayaz <gokalpayaz@windowslive.com>
2023-11-20 13:30:45 +0300

    StoryList component created to improve code reuse

--- dutluk/dutluk_frontend/src/components/AllStories.js
+++ dutluk/dutluk_frontend/src/components/AllStories.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import "./css/AllStories.css";
+import StoryList from "./StoryList";
 
 function AllStories() {
   const [allStories, setAllStories] = useState([]);
@@ -35,61 +36,7 @@ function AllStories() {
     <div className="all-stories">
       <h1>All Stories</h1>
       {allStories.map((story) => (
-        <div key={story.id} className="story">
-          <h2 className="story-title">
-            <a href={"/story/" + story.id}>{story.title}</a>
-          </h2>
-          <p className="story-details">
-            <b>Likes:</b> {story.likes ? story.likes.length : 0}
-          </p>
-          <p className="story-details">
-            <b>Labels:</b>{" "}
-            {story.labels.map((label, index) => (
-              <span key={index}>
-                <a href={"/story/search/label/" + label}>{label}</a>
-                {index < story.labels.length - 1 && ", "}
-              </span>
-            ))}
-          </p>
-          <p className="story-details">
-            <b>Written by:</b>{" "}
-            <a href={"/user/" + story.user.id}>{story.user.username}</a>
-          </p>
-
-          {story.startTimeStamp && (
-            <p className="story-details">
-              <b>Start Date:</b> {story.startTimeStamp}
-            </p>
-          )}
-          {story.endTimeStamp && (
-            <p className="story-details">
-              <b>End Date:</b> {story.endTimeStamp}
-            </p>
-          )}
-          {story.season && (
-            <p className="story-details">
-              <b>Season:</b> {story.season}
-            </p>
-          )}
-          {story.decade && (
-            <p className="story-details">
-              <b>Decade:</b> {story.decade}
-            </p>
-          )}
-
-          <p className="story-details">
-            <b>Published at:</b> {formatDate(story.createdAt)}
-          </p>
-
-          <p className="story-details">
-            <b>Locations:</b>
-          </p>
-          <ul className="locations-list">
-            {story.locations.map((location) => (
-              <li key={location.id}>{location.locationName}</li>
-            ))}
-          </ul>
-        </div>
+        StoryList(story={story})
       ))}
     </div>
   );
---
+++ dutluk/dutluk_frontend/src/components/StoryList.js
@@ -0,0 +1,73 @@
+const StoryList = ({ story, children }) => {
+    return (
+        <div key={story.id} className="story">
+            <h2 className="story-title">
+                <a href={"/story/" + story.id}>{story.title}</a>
+            </h2>
+            <p className="story-details">
+                <b>Likes:</b> {story.likes ? story.likes.length : 0}
+            </p>
+            <p className="story-details">
+                <b>Labels:</b>{" "}
+                {story.labels.map((label, index) => (
+                    <span key={index}>
+                        <a href={"/story/search/label/" + label}>{label}</a>
+                        {index < story.labels.length - 1 && ", "}
+                    </span>
+                ))}
+            </p>
+            <p className="story-details">
+                <b>Written by:</b>{" "}
+                <a href={"/user/" + story.user.id}>{story.user.username}</a>
+            </p>
+
+            {story.startTimeStamp && (
+                <p className="story-details">
+                    <b>Start Date:</b> {story.startTimeStamp}
+                </p>
+            )}
+            {story.endTimeStamp && (
+                <p className="story-details">
+                    <b>End Date:</b> {story.endTimeStamp}
+                </p>
+            )}
+            {story.season && (
+                <p className="story-details">
+                    <b>Season:</b> {story.season}
+                </p>
+            )}
+            {story.decade && (
+                <p className="story-details">
+                    <b>Decade:</b> {story.decade}
+                </p>
+            )}
+
+            <p className="story-details">
+                <b>Published at:</b> {formatDate(story.createdAt)}
+            </p>
+
+            <p className="story-details">
+                <b>Locations:</b>
+            </p>
+            <ul className="locations-list">
+                {story.locations.map((location) => (
+                    <li key={location.id}>{location.locationName}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    const hours = date.getHours();
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+
+    const formattedDate = `${day}/${month}/${year} ${hours}:${minutes}`;
+
+    return formattedDate;
+  }
+  
+export default StoryList;
