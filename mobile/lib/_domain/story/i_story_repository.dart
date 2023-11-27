778aad98f2e21d880351117fd7d2f37e1fa5bec3
amineglr <aminedglrglr@gmail.com>
2023-11-27 10:42:29 +0300

    near by stories feature for mobile app #98

--- mobile/lib/_domain/story/i_story_repository.dart
+++ mobile/lib/_domain/story/i_story_repository.dart
@@ -1,6 +1,7 @@
 import 'package:swe/_core/classes/typedefs.dart';
 import 'package:swe/_domain/story/model/addStory_model.dart';
 import 'package:swe/_domain/story/model/comment_model.dart';
+import 'package:swe/_domain/story/model/getNearbyStories_model.dart';
 import 'package:swe/_domain/story/model/postComment_model.dart';
 import 'package:swe/_domain/story/model/story_model.dart';
 
@@ -8,6 +9,8 @@ abstract interface class IStoryRepository {
   EitherFuture<List<StoryModel>> getAllStory();
   EitherFuture<List<StoryModel>> getActivityFeed();
   EitherFuture<List<StoryModel>> getFallowedStories();
+  EitherFuture<List<StoryModel>> getNearbyStories(GetNearbyStoriesModel model);
+
   EitherFuture<List<StoryModel>> myStories();
   EitherFuture<bool> deleteStory(int storyId);
   EitherFuture<StoryModel> getStoryDetail(int storyId);
