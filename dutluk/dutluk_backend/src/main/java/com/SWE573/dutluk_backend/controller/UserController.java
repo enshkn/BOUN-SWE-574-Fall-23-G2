5e687c9005f1d423a19660ebb62c902f39f6e65a
Hasan Deniz Dogan <hasandenizdogan@gmail.com>
2023-12-06 15:15:18 +0300

    Login exception handling fortified #329

--- dutluk/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/controller/UserController.java
+++ dutluk/dutluk_backend/src/main/java/com/SWE573/dutluk_backend/controller/UserController.java
@@ -58,7 +58,7 @@ public class UserController {
                                    HttpServletRequest request,
                                    HttpServletResponse response) throws HttpMessageNotWritableException {
         if(!userService.existsByEmail(loginRequest.getIdentifier()) && !userService.existsByUsername(loginRequest.getIdentifier())){
-                return new ResponseEntity<>("User with identifier "+loginRequest.getIdentifier()+" not found", HttpStatus.NOT_FOUND);
+                return IntegrationService.mobileCheck(request.getHeader("User-Agent"),"User with identifier "+loginRequest.getIdentifier()+" not found");
         }
         else{
             try {
