3654935fb3796e93f5009a9934d5e686dd8c515c
Görkem Kuyucu <mgorkemkuyucu@gmail.com>
2023-12-26 23:30:58 +0300

    Follow fetch bug fix closes #518

--- dutluk/dutluk_frontend/src/components/Profile.js
+++ dutluk/dutluk_frontend/src/components/Profile.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useParams } from "react-router-dom";
 import { Space, message } from 'antd';
 import photoPreview from "../profile_pic.png";
@@ -9,33 +9,26 @@ function Profile() {
   const { id } = useParams();
   const [user, setUser] = useState(null);
   const [isFollowing, setIsFollowing] = useState();
-  const [followButtonName, setFollowButtonName] = useState();
-  const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
   const [messageApi, contextHolder] = message.useMessage();
 
-  useEffect(() => {
-    axios
-      .get(
-        `${process.env.REACT_APP_BACKEND_URL}/api/user/profile`,
-        {
-          withCredentials: true,
-        }
-      )
-      .then((response) => {
-        const loggedInUser = response.data;
-        if (loggedInUser.following && loggedInUser.following.includes(id)) {
-          setFollowButtonName("Unfollow");
-          setIsFollowing(true);
-        } else {
-          setFollowButtonName("Follow");
-          setIsFollowing(false);
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        messageApi.open({ type: "error", content: "Error occured while loading your profile!"});
-      });
+  const fetchFollowStatus = useCallback(async () => {
+    try {
+        const response = await axios.get( 
+            `${process.env.REACT_APP_BACKEND_URL}/api/user/isFollowing/${id}`,
+            {
+                withCredentials: true,
+            }
+        );
+        setIsFollowing(response.data);  
+    } catch (error) {
+        console.error('Save status API error:', error.message);
+        messageApi.open({ type: "error", content: "Error occurred while fetching followed user data!" });
+    }
   }, [id, messageApi]);
+
+  useEffect(() => {
+    fetchFollowStatus();
+  }, [fetchFollowStatus]);
 
   useEffect(() => {
     axios
@@ -49,39 +42,24 @@ function Profile() {
         console.log(error);
         messageApi.open({ type: "error", content: "Error occured while loading the profile!"});
       });
-  }, [id, BACKEND_URL, messageApi]);
+  }, [id, messageApi]);
 
-  const handleFollowClick = () => {
-    if (isFollowing) {
-      axios
-        .post(
-          `${process.env.REACT_APP_BACKEND_URL}/api/user/follow/`,
-          { userId: id },
-          { withCredentials: true }
-        )
-        .then((response) => {
-          setFollowButtonName("Follow");
-          setIsFollowing(false);
-        })
-        .catch((error) => {
-          console.log(error);
-          messageApi.open({ type: "error", content: "Error occured while trying to unfollow this user!"});
-        });
-    } else {
-      axios
-        .post(
+  const handleFollowClick = async () => {
+    try {
+        const response = await axios.post(
           `${process.env.REACT_APP_BACKEND_URL}/api/user/follow`,
           { userId: id },
           { withCredentials: true }
-        )
-        .then((response) => {
-          setFollowButtonName("Unfollow");
-          setIsFollowing(true);
-        })
-        .catch((error) => {
-          console.log(error);
-          messageApi.open({ type: "error", content: "Error occured while trying to follow this user!"});
-        });
+        );
+        setIsFollowing(!isFollowing);
+        console.log(response);
+        if (isFollowing === true) {
+            messageApi.open({ type: "success", content: "You unfollowed the user" });
+        } else if (isFollowing === false) {
+            messageApi.open({ type: "success", content: "You followed the user" });
+        }
+    } catch (error) {
+        console.error(error);
     }
   };
 
@@ -115,7 +93,7 @@ function Profile() {
           <h1 className="profile-username">Username: {user.username}</h1>
           <p className="profile-biography">Biography: {user.biography}</p>
           <button className="follow-button" onClick={handleFollowClick}>
-            {followButtonName}
+            {isFollowing ? 'Unfollow' : 'Follow'}
           </button>
         </div>
       </Space>
