8c1b1259e8d82c1e995110d26d802634483f1dfd
Görkem Kuyucu <mgorkemkuyucu@gmail.com>
2024-01-01 23:18:12 +0300

    Loading stories from the related profile's user is completed closes #601

--- dutluk/dutluk_frontend/src/components/Profile.js
+++ dutluk/dutluk_frontend/src/components/Profile.js
@@ -4,12 +4,15 @@ import { useParams } from "react-router-dom";
 import { Space, message } from 'antd';
 import photoPreview from "../profile_pic.png";
 import "./css/Profile.css";
+import "./css/AllStories.css";
+import StoryList from "./StoryList";
 
 function Profile() {
   const { id } = useParams();
   const [user, setUser] = useState(null);
   const [isFollowing, setIsFollowing] = useState();
   const [messageApi, contextHolder] = message.useMessage();
+  const [userStories, setUserStories] = useState([]);
 
   const fetchFollowStatus = useCallback(async () => {
     try {
@@ -63,6 +66,23 @@ function Profile() {
     }
   };
 
+  useEffect(() => {
+    axios
+      .get(
+        `${process.env.REACT_APP_BACKEND_URL}/api/story/fromUserId/${id}`,
+        {
+          withCredentials: true,
+        }
+      )
+      .then((response) => {
+        setUserStories(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        messageApi.open({ type: "error", content: "Error occured while loading this user's stories"});
+      });
+  }, [messageApi]);
+
   if (!user) {
     return <div>User not found!</div>;
   } else {
@@ -95,6 +115,14 @@ function Profile() {
           <button className="follow-button" onClick={handleFollowClick}>
             {isFollowing ? 'Unfollow' : 'Follow'}
           </button>
+        </div>
+        <div className="all-stories">
+          <center><h1>User's Stories</h1></center>
+          {userStories.map((story) => (
+            <StoryList 
+              story={story} key={story.id}>
+            </StoryList>
+          ))}
         </div>
       </Space>
     );
